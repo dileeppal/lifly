7921d3eb2d47555843c0f2dd9562ad905dd58410
Dileep <pal.dileep1991@gmail.com>
2021-10-27 17:01:10 +0530

    product listing page

--- js/custom.js
+++ js/custom.js
@@ -5,4 +5,19 @@ jQuery(document).ready(function(){
     jQuery('.nav-toggle').click(function(){
         jQuery('body').toggleClass('open');
     });
+    jQuery(document).on('click', '.add' ,function () {
+		if (jQuery(this).prev().val() < 30) {
+    	jQuery(this).prev().val(+jQuery(this).prev().val() + 1);
+		}
+    });
+    jQuery(document).on('click', '.sub' ,function () {
+        if (jQuery(this).next().val() > 1) {
+            if (jQuery(this).next().val() > 1) jQuery(this).next().val(+jQuery(this).next().val() - 1);
+        } else {
+            jQuery(this).parents().closest('.product-card').removeClass('added-to-cart');     
+        }
+    });
+    jQuery(document).on('click', '.add-to-cart', function(){
+        jQuery(this).parents().closest('.product-card').addClass('added-to-cart');
+    });
 });
